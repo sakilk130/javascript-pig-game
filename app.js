db3f795c25b9c892616f04a7d0f7abb2db0e99ef
sakilk130 <sakilk130@gmail.com>
2020-08-23 21:55:57 +0600

    second commit

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score, activePlayer, dice;
+var score, activePlayer;
 score = [0, 0];
 roundScore = 0;
 activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
-document.querySelector("#score-" + activePlayer).textContent = dice;
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// document.querySelector("#score-" + activePlayer).textContent = dice;
+// var x = document.querySelector("#score-0").textContent;
+// console.log(x);
+
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var dice = Math.floor(Math.random() * 6) + 1;
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
