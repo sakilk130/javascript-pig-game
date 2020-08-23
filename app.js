cf593725a71b710bdf9a2b65457b8bceb8c60927
sakilk130 <sakilk130@gmail.com>
2020-08-24 00:49:29 +0600

    third commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ GAME RULES:
 var score, activePlayer;
 score = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector(".dice").style.display = "none";
 
@@ -22,12 +22,22 @@ document.getElementById("current-1").textContent = 0;
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var dice = Math.floor(Math.random() * 6) + 1;
+
   var diceDOM = document.querySelector(".dice");
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
-});
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
 
-// dice = Math.floor(Math.random() * 6) + 1;
-// document.querySelector("#score-" + activePlayer).textContent = dice;
-// var x = document.querySelector("#score-0").textContent;
-// console.log(x);
+    document.querySelector("#current-0").textContent = 0;
+    document.querySelector("#current-1").textContent = 0;
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+  }
+});
