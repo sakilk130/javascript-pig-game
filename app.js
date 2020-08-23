1941aa04730ba2077ad7015780cb8b6d62c880e8
sakilk130 <sakilk130@gmail.com>
2020-08-24 01:26:02 +0600

    fourth commit

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
+
 var score, activePlayer;
 score = [0, 0];
 roundScore = 0;
@@ -30,14 +29,36 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore += dice;
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-
-    document.querySelector("#current-0").textContent = 0;
-    document.querySelector("#current-1").textContent = 0;
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-    document.querySelector(".dice").style.display = "none";
+    nextPlayer();
   }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  score[activePlayer] += roundScore;
+  document.querySelector("#score-" + activePlayer).textContent =
+    score[activePlayer];
+  if (score[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("winner");
+  } else {
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.querySelector("#current-0").textContent = 0;
+  document.querySelector("#current-1").textContent = 0;
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".dice").style.display = "none";
+}
